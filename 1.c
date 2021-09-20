a1abb1d8f0e06ee7450faa6a01d12326c62e6b95
Sergey <sorokin.sns@yandex.ru>
2021-09-20 22:29:40 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -6,7 +6,7 @@
 
 #include <stdlib.h>
 
-#pragma warning(disable: 4996)
+//#pragma warning(disable: 4996)
 
 #define TRUE 1
 #define FALSE 0
@@ -17,30 +17,49 @@ int main() {
     int value = 0;
     int count = 0;
     int sign = FALSE;
-    while (((buffer = getchar()) != EOF) && buffer != '\n' && buffer != '\0') {
+    while (((buffer = getchar()) != EOF) && buffer != '\n' && buffer != '\0') 
+    {
         if ((buffer == '-') && (sign == FALSE)) {
             sign = TRUE;
         }
         else {
 
-            if ((buffer == '1') || (buffer == '0')) {
+            if ((buffer == '1') || (buffer == '0')) 
+            {
                 value <<= 1;
                 value += buffer - '0';
                 count++;
             }
-            else {
+            else 
+            {
                 printf("Error.\n");
                 exit(ERROR);
             }
+            }
+    }
+   printf("rrr \n %i \n", count);
+    if (sign == TRUE) 
+    {
+
+        if (count > sizeof(int) * CHAR_BIT) 
+        {
+            printf("Error oversize.\n");
+            exit(ERROR);
         }
     }
-    if (count >= sizeof(int) * CHAR_BIT) {
-        printf("Error oversize.\n");
-        exit(ERROR);
+    else 
+    {
+        if (count >= sizeof(int) * CHAR_BIT) 
+        {
+            printf("Error oversize.\n");
+            exit(ERROR);
+        }
     }
+    
+ //1111 1111 1111 1111 1111 1111 1111 1111
 
     if (sign == TRUE)
         value = value * (-1);
-    printf("%i", value);
+    printf("%i \n", value);
     return 0;
 }
